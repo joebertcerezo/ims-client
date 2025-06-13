dd7ac5ebce22c8adaac56ca03c08906717571bf4
sirezo <joebertcerezo@gmail.com>
2025-06-13 09:44:53 +0800

    Create signin.vue

---
+++ pages/signin.vue
@@ -0,0 +1,20 @@
+<template>
+  <div
+    class="bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10"
+  >
+    <div class="flex w-full max-w-sm flex-col gap-6">
+      <a href="#" class="flex items-center gap-2 self-center font-medium">
+        Inventory Management
+      </a>
+      <SigninForm />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+export const description = "A login page with a muted background color.";
+</script>
+
+<script setup lang="ts">
+import SigninForm from "~/components/SigninForm.vue";
+</script>

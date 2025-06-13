0a736c0a4b2ddd026bbae6f5f9e5ea80007b9143
sirezo <joebertcerezo@gmail.com>
2025-06-13 09:44:20 +0800

    Create SigninForm.vue

---
+++ components/SigninForm.vue
@@ -0,0 +1,70 @@
+<template>
+  <div :class="cn('flex flex-col gap-6', props.class)">
+    <Card>
+      <CardHeader class="text-center">
+        <CardTitle class="text-xl"> Welcome back </CardTitle>
+      </CardHeader>
+      <CardContent>
+        <form>
+          <div class="grid gap-6">
+            <div class="grid gap-6">
+              <div class="grid gap-3">
+                <Label for="email">Email</Label>
+                <Input
+                  id="email"
+                  type="email"
+                  placeholder="m@example.com"
+                  required
+                />
+              </div>
+              <div class="grid gap-3">
+                <div class="flex items-center">
+                  <Label for="password">Password</Label>
+                  <a
+                    href="#"
+                    class="ml-auto text-sm underline-offset-4 hover:underline"
+                  >
+                    Forgot your password?
+                  </a>
+                </div>
+                <Input id="password" type="password" required />
+              </div>
+              <Button type="submit" class="w-full"> Login </Button>
+            </div>
+            <div class="text-center text-sm">
+              Don't have an account?
+              <NuxtLink to="/signup" class="underline underline-offset-4">
+                Sign up
+              </NuxtLink>
+            </div>
+          </div>
+        </form>
+      </CardContent>
+    </Card>
+    <div
+      class="text-muted-foreground *:[a]:hover:text-primary text-center text-xs text-balance *:[a]:underline *:[a]:underline-offset-4"
+    >
+      By clicking continue, you agree to our
+      <a href="#">Terms of Service</a> and <a href="#">Privacy Policy</a>.
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import type { HTMLAttributes } from "vue";
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+
+const props = defineProps<{
+  class?: HTMLAttributes["class"];
+}>();
+</script>

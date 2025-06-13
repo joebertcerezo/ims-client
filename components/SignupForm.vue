ba98a31e6a2b915d0206dfba6ce6ad981fda25db
sirezo <joebertcerezo@gmail.com>
2025-06-13 09:44:21 +0800

    Create SignupForm.vue

---
+++ components/SignupForm.vue
@@ -0,0 +1,62 @@
+<template>
+  <div :class="cn('flex flex-col gap-6', props.class)">
+    <Card>
+      <CardHeader class="text-center">
+        <CardTitle class="text-xl"> Create Account </CardTitle>
+      </CardHeader>
+      <CardContent>
+        <form>
+          <div class="grid gap-6">
+            <div class="grid gap-6">
+              <div class="grid gap-3">
+                <Label for="email">Email</Label>
+                <Input
+                  id="email"
+                  type="email"
+                  placeholder="m@example.com"
+                  required
+                />
+              </div>
+              <div class="grid gap-3">
+                <Label for="password">Password</Label>
+                <Input id="password" type="password" required />
+              </div>
+              <Button type="submit" class="w-full"> Login </Button>
+            </div>
+            <div class="text-center text-sm">
+              Already have an account?
+              <NuxtLink to="/signin" class="underline underline-offset-4">
+                Sign in
+              </NuxtLink>
+            </div>
+          </div>
+        </form>
+      </CardContent>
+    </Card>
+    <div
+      class="text-muted-foreground *:[a]:hover:text-primary text-center text-xs text-balance *:[a]:underline *:[a]:underline-offset-4"
+    >
+      By clicking continue, you agree to our
+      <a href="#">Terms of Service</a> and <a href="#">Privacy Policy</a>.
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import type { HTMLAttributes } from "vue";
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+
+const props = defineProps<{
+  class?: HTMLAttributes["class"];
+}>();
+</script>

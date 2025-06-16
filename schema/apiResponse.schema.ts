a50e1922e7fcdb4a1186fe150c1f909dfdaa7e98
sirezo <joebertcerezo@gmail.com>
2025-06-16 11:53:04 +0800

    Create apiResponse.schema.ts

---
+++ schema/apiResponse.schema.ts
@@ -0,0 +1,8 @@
+import { z } from 'zod'
+
+export const ApiResponseSchema = <T extends z.ZodTypeAny>(dataSchema: T) =>
+  z.object({
+    code: z.string(),
+    status: z.string(),
+    data: dataSchema,
+  });

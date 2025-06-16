d68da9ce3f5f684565212ad49f4dadd3119b527c
sirezo <joebertcerezo@gmail.com>
2025-06-16 11:52:58 +0800

    Create product.schema.ts

---
+++ schema/product.schema.ts
@@ -0,0 +1,15 @@
+import { z } from 'zod'
+import { ApiResponseSchema } from './apiResponse.schema'
+import { CategoryDataSchema } from './category.schema'
+
+export const ProductDataSchema = z.object({
+  id: z.string().uuid(),
+  productName: z.string(),
+  quantity: z.number(),
+  category: CategoryDataSchema,
+  createdAt: z.string()
+})
+
+export const ProductListSchema = ApiResponseSchema(z.array(ProductDataSchema))
+
+

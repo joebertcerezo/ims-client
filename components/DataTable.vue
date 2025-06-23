acef47280cb6d28c7032dc06569c1fba6100b81e
Janlouegie O. Balbon <jobalbon@vauldex.com>
2025-06-23 11:41:21 +0800

    changed Alert to sonner toast in deleting product

--- components/DataTable.vue
+++ components/DataTable.vue
@@ -37,16 +37,46 @@
                   <Edit class="h-4 w-4" />
                 </Button>
               </EditProductForm>
-
-              <Button
-                class="hover:cursor-pointer"
-                variant="ghost"
-                size="sm"
-                v-if="currentUser.data?.role === 'admin'"
-                @click="deleteItem(product.id, product.productName)"
-              >
-                <Trash2 class="h-4 w-4 text-destructive" />
+              <Dialog>
+                <DialogTrigger as-child>
+                  <Button 
+                  class="hover:cursor-pointer"
+                  variant="ghost"
+                  size="sm"
+                  v-if="currentUser.data?.role === 'admin'">
+                  <Trash2 class="h-4 w-4 text-destructive" />
               </Button>
+                </DialogTrigger>
+                <DialogContent>
+                  <DialogHeader>
+                    <DialogTitle>
+                      Delete this item?
+                    </DialogTitle>
+                    <DialogDescription>
+                      Are you sure you want to delete <span class="font-bold"> {{ product.productName }} </span>?
+                    </DialogDescription>
+                  </DialogHeader>
+                  <DialogFooter class="sm:justify-end">
+                    <DialogClose as-child>
+                      <Button type="button" variant="outline">
+                        Close
+                      </Button>
+                      <Button type="button" variant="destructive" 
+                      @click="() => {
+                        deleteItem(product.id, product.productName),
+                        toast(toastMsg, {
+                          style:{
+                            background: toastStyle
+                          } 
+                        })
+                        ;}"
+                      >
+                        Delete
+                      </Button>
+                    </DialogClose>
+                  </DialogFooter>
+                </DialogContent>
+              </Dialog>
             </div>
           </TableCell>
         </TableRow>
@@ -69,6 +99,7 @@ import {
 import { Button } from "@/components/ui/button";
 import { Edit, Trash2, CirclePlus } from "lucide-vue-next";
 import { ProductResponseSchema, ProductListSchema } from "~/schema/product.schema";
+import { toast } from 'vue-sonner'
 
 const { public: { apiUrl: API_URL } } = useRuntimeConfig();
 
@@ -79,6 +110,8 @@ const emit = defineEmits<{
 }>();
 
 const productsData = ref<ProductListResponse>();
+const toastMsg = ref('')
+const toastStyle = ref('')
 
 const productsDataGet = async () => {
   try {
@@ -103,7 +136,7 @@ const formatDate = (dateString: string) => {
 };
 
 const deleteItem = async (id: string, productName: string) => {
-  if (confirm(`Are you sure you want to delete item ${productName}?`)) {
+  // if (confirm(`Are you sure you want to delete item ${productName}?`)) {
     try {
       const data = await $fetch(`${API_URL}/api/products`, {
         method: "DELETE",
@@ -117,13 +150,16 @@ const deleteItem = async (id: string, productName: string) => {
             (product: ProductData) => product.id === id
           );
           if (index !== -1) productsData.value?.data.splice(index!, 1);
-          window.alert("Product deleted successfully");
+          toastStyle.value = '#73EC8B'
+          toastMsg.value = 'Product deleted successfully.'
         }
       }
     } catch (err) {
-      console.error("An error occurred.");
+      console.error(err);
+      toastMsg.value = 'Something went wrong. Try again later.'
+      toastStyle.value = '#FF4848'
     }
-  }
+  // }
 };
 
 onMounted(async () => {
